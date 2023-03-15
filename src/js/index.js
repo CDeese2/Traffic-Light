a3b90c40d5e837bb7f5749f1403a47d37fe38f76
CDeese2 <cdeese2@gmail.com>
2023-03-15 22:29:33 +0000

    traffic light done!

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,29 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import TrafficLight from "./component/trafficLight.jsx";
+// import Light from "./component/Light.jsx"
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+function App() {
+    return (
+      <div className="App">
+        <TrafficLight initialValue={0} />
+        <TrafficLight initialValue={1} />
+      </div>
+    );
+  }
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
+ReactDOM.render(<App />, document.querySelector("#app"));
+
+
+
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
+
+// REFERENCES:
+// https://codesandbox.io/s/traffic-light-using-hooks-xlw615w7ow?file=/src/TrafficLight.jsx:534-698
+// https://codesandbox.io/s/ox19m9r23q?file=/src/index.js
